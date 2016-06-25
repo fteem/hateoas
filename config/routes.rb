528e7cc95c5a0805bfba9e9f9e8871c71f1c27ba
Ilija Eftimov <ileeftimov@gmail.com>
2016-06-25 22:47:43 +0200

    Nest Accounts route under Author

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :authors
-  resources :posts
+  resources :authors do
+    resources :articles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
